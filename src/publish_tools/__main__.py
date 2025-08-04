b1e150992073221c77bfee2752f344ca290fe236
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-08-04 16:16:20 +0200

    switch to models

--- src/publish_tools/__main__.py
+++ src/publish_tools/__main__.py
@@ -1,8 +1,10 @@
 import json
 from pathlib import Path
 
+from .models import Edition, Guide
 
-def get_package_information(project_dir: Path):
+
+def get_package_information(project_dir: Path) -> Guide:
     output_dir = project_dir / "output"
 
     ig_file = (
@@ -19,21 +21,24 @@ def get_package_information(project_dir: Path):
 
     ig_info = json.loads(ig_file.read_text(encoding="utf-8"))
     pub_info = json.loads(pub_file.read_text(encoding="utf-8"))
-    info = {
-        "name": pub_info["title"],
-        "category": pub_info["category"],
-        "npm-name": pub_info["package-id"],
-        "description": pub_info["introduction"],
-        "canonical": ig_info["url"].rsplit("/", 2)[0],
-        "ci-build": pub_info["ci-build"],
-        "edition": {
-            "name": pub_info["sequence"],
-            "ig-version": pub_info["version"],
-            "package": f"{pub_info["package-id"]}#{pub_info["version"]}",
-            "fhir-version": ig_info["fhirVersion"],
-            "url": pub_info["path"],
-            "description": pub_info["desc"],
-        },
-    }
+
+    info = Guide(
+        name=pub_info["title"],
+        category=pub_info["category"],
+        npm_name=pub_info["package-id"],
+        description=pub_info["introduction"],
+        canonical=ig_info["url"].rsplit("/", 2)[0],
+        ci_build=pub_info["ci-build"],
+        editions=[
+            Edition(
+                name=pub_info["sequence"],
+                ig_version=pub_info["version"],
+                package=f"{pub_info["package-id"]}#{pub_info["version"]}",
+                fhir_version=ig_info["fhirVersion"],
+                url=pub_info["path"],
+                description=pub_info["desc"],
+            )
+        ],
+    )
 
     return info
---
+++ src/publish_tools/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+
+
+class Edition(BaseModel):
+    name: str
+    ig_version: str = Field(serialization_alias="ig-version")
+    package: str
+    fhir_version: list[str] = Field(serialization_alias="fhir-version")
+    url: str
+    description: str
+
+
+class Guide(BaseModel):
+    name: str
+    category: str
+    npm_name: str = Field(serialization_alias="npm-name")
+    description: str
+    canonical: str
+    ci_build: str = Field(serialization_alias="ci-build")
+    editions: list[Edition]
