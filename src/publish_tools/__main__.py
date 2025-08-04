704aa64410d17073490b2054c4b81ff065b9b1d9
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-08-04 15:49:06 +0200

    start getting package information

---
+++ src/publish_tools/__main__.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+
+
+def get_package_information(project_dir: Path):
+    output_dir = project_dir / "output"
+
+    ig_file = (
+        res[0]
+        if len(res := list(output_dir.glob("ImplementationGuide*.json"))) == 1
+        else None
+    )
+    if ig_file is None:
+        raise Exception("package not built")
+
+    pub_file = project_dir / "publication-request.json"
+    if pub_file is None:
+        raise Exception("publication request missing")
+
+    ig_info = json.loads(ig_file.read_text(encoding="utf-8"))
+    pub_info = json.loads(pub_file.read_text(encoding="utf-8"))
+    info = {
+        "name": pub_info["title"],
+        "category": pub_info["category"],
+        "npm-name": pub_info["package-id"],
+        "description": pub_info["introduction"],
+        "canonical": ig_info["url"].rsplit("/", 2)[0],
+        "ci-build": pub_info["ci-build"],
+        "edition": {
+            "name": pub_info["sequence"],
+            "ig-version": pub_info["version"],
+            "package": f"{pub_info["package-id"]}#{pub_info["version"]}",
+            "fhir-version": ig_info["fhirVersion"],
+            "url": pub_info["path"],
+            "description": pub_info["desc"],
+        },
+    }
+
+    return info
