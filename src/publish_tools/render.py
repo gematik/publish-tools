9e60b0feeabaa0086614ada6e7f288251a2f00fc
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-08-05 15:56:45 +0200

    render ig list

--- src/publish_tools/render.py
+++ src/publish_tools/render.py
@@ -2,14 +2,26 @@ from pathlib import Path
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-from .models import Guide
+from .models import Guide, IgList
 
 
 def render_history(file: Path):
     content = file.read_text(encoding="utf-8")
     history = Guide.model_validate_json(content)
 
-    data = {"guides": [history.model_dump()]}
+    data = {"title": f"{history.name} History", "guides": [history.model_dump()]}
+    content = _render(data)
+
+    output = file.with_name("index.html")
+    output.write_text(content, encoding="utf-8")
+
+
+def render_ig_list(file: Path):
+    content = file.read_text(encoding="utf-8")
+    ig_list = IgList.model_validate_json(content)
+
+    data = ig_list.model_dump()
+    data["title"] = "IG List"
     content = _render(data)
 
     output = file.with_name("index.html")
