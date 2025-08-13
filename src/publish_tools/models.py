adc984244d9cf05e16d36e54104415fa53c9c4cb
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-08-13 11:33:36 +0200

    model for RSS feed

--- src/publish_tools/models.py
+++ src/publish_tools/models.py
@@ -1,4 +1,15 @@
-from pydantic import AliasChoices, BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+from pydantic import AliasChoices, BaseModel, Field, field_serializer
+from pydantic_xml import BaseXmlModel, attr, element
+
+DATETIME_FORMAT = "%a, %d %b %Y %H:%M:%S %z"
+
+NS_ATOM = "http://www.w3.org/2005/Atom"
+NS_CONTENT = "http://purl.org/rss/1.0/modules/content/"
+NS_DC = "http://purl.org/dc/elements/1.1/"
+NS_FHIR = "http://hl7.org/fhir/feed"
 
 
 class IgDef(BaseModel):
@@ -41,3 +52,68 @@ class Guide(IgDef):
 
 class IgList(BaseModel):
     guides: list[Guide] = []
+
+
+######
+# Package RSS Feed
+######
+class PackageGuid(BaseXmlModel):
+    is_perma_link: bool = attr(name="isPermaLink", default=True)
+    url: str
+
+
+class PackageItem(BaseXmlModel, nsmap={"dc": NS_DC, "fhir": NS_FHIR}):
+    title: str = element()
+    description: str = element()
+    link: str = element()
+    guid: PackageGuid = element()
+    creator: str = element(ns="dc")
+    fhir_version: str = element(tag="version", ns="fhir")
+    fhir_kind: str = element(tag="kind", ns="fhir", default="IG")
+    pub_date: datetime = element(tag="pubDate")
+    details: Optional[str] = element(ns="fhir", default=None)
+
+    @field_serializer("pub_date")
+    def encode_pub_date(self, value: datetime) -> str:
+        return value.strftime(DATETIME_FORMAT)
+
+
+class PackageLink(BaseXmlModel, ns="atom"):
+    href: str = attr()
+    rel: str = attr(default="self")
+    type: str = attr(default="application/rss+xml")
+
+
+class PackageChannel(BaseXmlModel, nsmap={"atom": NS_ATOM}):
+    title: str = element()
+    description: str = element()
+    link: str = element()
+    generator: str = element()
+    last_build_date: datetime = element(tag="lastBuildDate")
+    atom_link: PackageLink = element(tag="link", ns="atom")
+    pub_date: datetime = element(tag="pubDate")
+    language: str = element(default="en")
+    ttl: int = element(default=600)
+    item: list[PackageItem] = element()
+
+    @field_serializer("last_build_date")
+    def encode_last_build_date(self, value: datetime) -> str:
+        return value.strftime(DATETIME_FORMAT)
+
+    @field_serializer("pub_date")
+    def encode_pub_date(self, value: datetime) -> str:
+        return value.strftime(DATETIME_FORMAT)
+
+
+class PackageFeed(
+    BaseXmlModel,
+    tag="rss",
+    nsmap={
+        "atom": NS_ATOM,
+        "content": NS_CONTENT,
+        "dc": NS_DC,
+        "fhir": NS_FHIR,
+    },
+):
+    channel: PackageChannel = element()
+    version: str = attr(default="2.0")
