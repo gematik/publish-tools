699e3c82b86252ce26416e737b8864ead9a56375
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-11-03 10:05:14 +0100

    clean-up

--- src/publish_tools/ig.py
+++ src/publish_tools/ig.py
@@ -59,43 +59,8 @@ def publish(project_dir: Path, ig_registry_dir: Path):
     pub_dir = project_dir / "publish"
     pub_dir.mkdir(parents=True, exist_ok=True)
 
-    # output_dir = project_dir / "output"
-
     pub_project = info.canonical.rsplit("/", 1)[1]
     pub_ig_dir = pub_dir / pub_project
-
-    ###
-    # Do not create versioned copies of the generated IG
-    ###
-
-    # pub_ig_version_dir = pub_ig_dir / info.edition.ig_version
-    # pub_ig_version_dir.mkdir(parents=True, exist_ok=True)
-
-    # # Clear previous content
-    # if pub_ig_dir.exists():
-    #     shutil.rmtree(pub_ig_version_dir)
-    #     log_info("removed previous versioned IG")
-
-    # shutil.copytree(output_dir, pub_ig_version_dir)
-    # log_succ("created versioned IG")
-
-    ######
-    # Create archive
-    ######
-    # archive_dir = pub_dir / "ig-build-zips"
-    # archive_dir.mkdir(parents=True, exist_ok=True)
-
-    # archive = Path(
-    #     shutil.make_archive(
-    #         base_name=info.edition.package,
-    #         format="zip",
-    #         root_dir=pub_ig_version_dir.parent,
-    #         base_dir=pub_ig_version_dir.name,
-    #     )
-    # )
-
-    # shutil.move(archive, archive_dir / archive.name)
-    # log_succ("created IG archive")
 
     # Update history file
     history_file = ig_history.update(pub_ig_dir, info)
