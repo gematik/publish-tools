214a5815c2a277977f3649068f6b27201faf705c
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-11-03 11:30:35 +0100

    model for package list

---
+++ src/publish_tools/models/package_list.py
@@ -0,0 +1,34 @@
+from datetime import date as datetime_date
+from typing import List
+
+from pydantic import AliasChoices, AnyUrl, BaseModel, Field
+
+
+class PackageListSimpleEntry(BaseModel):
+    version: str
+    desc: str
+    path: AnyUrl
+    status: str
+    current: bool
+
+
+class PackageListCurrentEntry(PackageListSimpleEntry):
+    current: bool = True
+
+
+class PackageListSpecificEntry(PackageListSimpleEntry):
+    date: datetime_date
+    sequence: str
+    fhirversion: str
+    current: bool = False
+
+
+class PackageList(BaseModel):
+    package_id: str = Field(
+        serialization_alias="package-id",
+        validation_alias=AliasChoices("package_id", "package-id"),
+    )
+    canonical: AnyUrl
+    title: str
+    introduction: str
+    list: List[PackageListCurrentEntry | PackageListSpecificEntry] = []
